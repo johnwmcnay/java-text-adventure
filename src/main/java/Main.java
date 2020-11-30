4e355d239564ed65fb8179fbf14751933a8da249
John McNay <john.w.mcnay@gmail.com>
2020-11-30 16:10:44 -0600

    output information for the starting point

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -6,6 +5,7 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -18,9 +18,27 @@ public class Main {
         Object obj = parser.parse(reader);
         HashMap<?,?> map = (HashMap<?, ?>) obj;
         HashMap<?,?> data = (HashMap<?, ?>) map.get("data");
+        HashMap<?,?> stitches = (HashMap<?, ?>) data.get("stitches");
+        HashMap<?,?> start = (HashMap<?, ?>) stitches.get(data.get("initial"));
+        List<?> list = (List<?>) start.get("content");
 
-        System.out.println("obj = " + obj);
-        System.out.println("map.get(\"data\") = " + data.get("initial"));
+        System.out.println("list = " + list);
+        
+        for (short i = 0; i < list.size(); i++) {
+            HashMap<?, ?> element;
 
+            if (i == 0) {
+                System.out.println(list.get(i));
+            } else {
+                element = (HashMap<?, ?>) list.get(i);
+                String option = (String) element.get("option");
+
+                if (option != null) {
+                    System.out.println(option);
+                }
+                
+            }
+
+        }
     }
 }
